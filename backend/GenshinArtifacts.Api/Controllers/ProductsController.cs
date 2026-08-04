1a86812d340ba541668fbaaf5dcf07b4a2dd94d9
Bryan Yao <bryanjerahmeelyao@gmail.com>
2026-08-04 23:20:46 +0800

    backend: Add ProductsController

---
+++ backend/GenshinArtifacts.Api/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using GenshinArtifacts.Api.Models;
+
+namespace GenshinArtifacts.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private static readonly List<Product> _products = new();
+    private static int _nextId = 1;
+}
