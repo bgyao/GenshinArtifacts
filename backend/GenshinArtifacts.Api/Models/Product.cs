3f6f40a7e642e7433e58e428f54d2df96b6bfbc8
Bryan Jerahmeel Yao <bryan.yao@rea-group.com>
2026-08-09 09:46:55 +0800

    GAD-18: Implement Get Product By Id

--- backend/GenshinArtifacts.Api/Models/Product.cs
+++ backend/GenshinArtifacts.Api/Models/Product.cs
@@ -10,8 +10,12 @@ public class Product
     [MaxLength(100)]
     public string Name { get; set; } = string.Empty;
 
+    /// <remarks>
+    /// Opted not to use [Range(0.01, double.MaxValue)] because decimal.MaxValue 
+    /// is too large for double.MaxValue.
+    /// </remarks>
     [Required]
-    [Range(typeof(decimal), "0.01", "79228162514264337593543950335")]
+    [Range(typeof(decimal), "0.01", "79228162514264337593543950335", ErrorMessage = "Price must be greater than 0.")]
     public decimal Price { get; set; }
 
     public bool IsActive { get; set; } = true;
